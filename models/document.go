49111a3114dd822c6fd4aace5124ed92d052d56a
Evgeniy Urvantsev <resmus@gmail.com>
2026-04-30 23:42:30 +0300

    Fix goimports formatting in models/document.go

--- models/document.go
+++ models/document.go
@@ -12,21 +12,21 @@ import "time"
 
 // Document represents a document in EVA system
 type Document struct {
-	ID              string    `json:"id"`
-	ClassName       string    `json:"class_name,omitempty"`
-	Code            string    `json:"code,omitempty"`
-	Name            string    `json:"name"`
-	Text            string    `json:"text,omitempty"`
-	ProjectID       string    `json:"project_id,omitempty"`
-	ParentID        string    `json:"parent_id,omitempty"`
-	CacheStatusType string    `json:"cache_status_type,omitempty"`
-	CmfCreatedAt    time.Time `json:"cmf_created_at,omitempty"`
-	CmfModifiedAt   time.Time `json:"cmf_modified_at,omitempty"`
-	CmfOwnerID       string `json:"cmf_owner_id,omitempty"`
-	CmfDeleted       bool   `json:"cmf_deleted,omitempty"`
-	OrderNo          int    `json:"orderno,omitempty"`
-	TreeNodeIsBranch bool   `json:"tree_node_is_branch,omitempty"`
-	WorkflowID       string `json:"workflow_id,omitempty"`
+	ID               string    `json:"id"`
+	ClassName        string    `json:"class_name,omitempty"`
+	Code             string    `json:"code,omitempty"`
+	Name             string    `json:"name"`
+	Text             string    `json:"text,omitempty"`
+	ProjectID        string    `json:"project_id,omitempty"`
+	ParentID         string    `json:"parent_id,omitempty"`
+	CacheStatusType  string    `json:"cache_status_type,omitempty"`
+	CmfCreatedAt     time.Time `json:"cmf_created_at,omitempty"`
+	CmfModifiedAt    time.Time `json:"cmf_modified_at,omitempty"`
+	CmfOwnerID       string    `json:"cmf_owner_id,omitempty"`
+	CmfDeleted       bool      `json:"cmf_deleted,omitempty"`
+	OrderNo          int       `json:"orderno,omitempty"`
+	TreeNodeIsBranch bool      `json:"tree_node_is_branch,omitempty"`
+	WorkflowID       string    `json:"workflow_id,omitempty"`
 }
 
 // DocumentResponse for Document.get (single document).
